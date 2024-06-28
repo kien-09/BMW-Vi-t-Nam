eaafdf1ba3aa364a5b5f21aa06eb01b8e5bfd3e7
kien-09 <trungkien102k3@gmail.com>
2024-06-28 23:19:12 +0700

    hoanthanh

---
+++ index.js
@@ -0,0 +1,131 @@
+let product = [
+    {
+        anh: "https://www.bmw.vn/content/dam/bmw/marketASIA/bmw_vn/navigation-model/new-x3-model-card.jpg",
+        ten: "BMW X3",
+        loai: "Xăng",
+        gia: "Giá từ 1.795.000.000 VNĐ"
+    },
+    {
+        anh: "https://www.bmw.vn/content/dam/bmw/marketASIA/bmw_vn/navigation-model/new-x4-model-card.jpg",
+        ten: "BMW X4",
+        loai: "Xăng",
+        gia: "Giá từ 2.899.000.000 VNĐ"
+    },
+    {
+        anh: "https://prod.cosy.bmw.cloud/bmwweb/cosySec?COSY-EU-100-7331cqgv2Z7d%25i02uCaY3MuO2kOHUtWPfbYfQJ7T10tLhu1XzWVo7puMLWFmdkAj5DOP7tIxZ8XgY1nTNIowJ4HO3zkyXq%25sGM8snGhMQSk%2508Xc9Vo74giUaNF1VgxNJ0%25ls2oub5imC2yRCzXeTt%25ViPRKVZVYVESWh1DMztIOdeqVYDafHAPjmztYRSJwb67aftxdTCyw1RSfWQlEn%25VxdSeZsNJuzWQdjc96k3aeZQ6KowqXRjcZwBLGwrx6Kc%252y9t4WwBKupCzXFe%252B3iEbVIjup2XH5Llv63iprJ8DaGwXHi4TPY09%25rJHFlMtLou4TJIsDvWL3FlTv0YGCyXIslGAtOJCrv0s9Ofe6E4GA0ogSjWNF9OALUdMKkIogOybQqonvLUgChZnA5GybUEqc5289ChbNmK4PPoEqhk7B%25iMLNmqn12ufDyk7m5VptRYCn178zm1rtE5V1Pa7IsfN8zVMR1KzSkPazDxVyrdnMRaYWB4EQ5DxRte26%25Z8YWxfjpGEcPteWS6iERKMfjedwHsNBDS6jQ%25J0p2Ydw6ZuQVAptvwsbT365emO%25v4WsxtjKqc1QSE0",
+        ten: "THE NEW X5",
+        loai: "Xăng",
+        gia: "Giá từ 3.909.000.000 VNĐ"
+    },
+    {
+        anh: "https://prod.cosy.bmw.cloud/bmwweb/cosySec?COSY-EU-100-7331cqgv2Z7d%25i02uCaY3MuO2kOHUtWPfbYf6GDP10tLhu1XzWVo7puMLWFmdkAj5DOPcjV%25Z8XgY1nTNIowJ4HO3zkyXq%25sGM8snGhMQSk%2508Xc9Vo74gi8WNF1VgxNJ0%25ls2oub5imC2yRCzXeTt%25ViPRKVZVYVbqWh1DMztIA1eqVYDaf4YQjmztYRSDXR67aftxdYraw1RSfWQt4S%25VxdSeZfq6uzWQdjcFNt3aeZQ6KI01XRjcZwBvLmrx6Kc%252GyF4WwBKup9C5Fe%252B3ibvRIjup2XHhDbv63iprJ8mDGwXHi4Tmtd9%25rJHFl5t3ou4TJIs8vSL3FlTv0PGeyXIslGAMwMCrv0s9ODGTE4GA0ogS8hNF9OALUdcnkIogOybQCunvLUgChZqA5GybUEqc6D89ChbNmKMVPoEqhk7BDDMLNmqn12cjDyk7m5VnJbYCn178zB9MtE5V1Pa29yfN8zVMRpokSkPazDxTY7dnMRaYWl%25TQ5DxRtesCZZ8YWxfjtzzcPteWS6fgoKMfjedwOCNBDS6jQ%25gEy2Ydw6ZuUIPptQ%25wc3llFifZu%25KXsnDHSc3uBr0FvJdKX324AIGTQBrXpFOvWlZ24riIg5kscpF4HvUwU0KiIFJG8frABHvIT9hqlO2JGvloqmWgpT9GsLD6jUilo90yYw0bHsLoACR1%25hJ0yLOEjgmqTACygNLa6mlOECUkait7sgNEbnbiy10UkNh56H4VAfke3x8NHC4wnfDyeLAEmrqIbgBj",
+        ten: "BMW X6",
+        loai: "Xăng",
+        gia: "Giá từ 3.799.000.000 VNĐ"
+    },
+    {
+        anh: "https://prod.cosy.bmw.cloud/bmwweb/cosySec?COSY-EU-100-7331cqgv2Z7d%25i02uCaY3MuO2kOHUtWPfbYfFd1v10tLhu1XzWVo7puMLWFmdkAj5DOP7IIBZ8XgY1nTNIowJ4HO3zkyXq%25sGM8snGhMQSk%2508Xc9Vo74gSQNNF1VgxNJ0%25ls2oub5imC2yRCzXeTt%25ViPRKVZVYVOVWh1DMztINpeqVYDafHLojmztYRSJyN67aftxdTCIw1RSfWQveE%25VxdSeZGjbuzWQdjcyF23aeZQ6KCkpXRjcZwBEn4rx6Kc%252Nu74WwBKupkm1Fe%252B3in7qIjup2XH5yDv63iprJ8mLGwXHi4TP7I9%25rJHFlM5hou4TJIsHkIL3FlTv0JnFyXIslGAnVNCrms8fnOl6JQD0mbH8irTod9cvFxP",
+        ten: "THE NEW BMW X7",
+        loai: "Xăng",
+        gia: "Giá từ 5.549.000.000 VNĐ"
+    },
+    {
+        anh: "https://prod.cosy.bmw.cloud/bmwweb/cosySec?COSY-EU-100-7331cqgv2Z7d%25i02uCaY3MuO2kOHUtWPfbYfQnPz10tLhu1XzWVo7puMLWFmdkAj5DOP7tFJZ8XgY1nTNIowJ4HO3zkyXq%25sGM8snGhMQSk%2508Xc9Vo74gijPNF1VgxNJ0%25ls2oub5imC2yRCzXeTt%25ViPRKVZVYVfFWh1DMzti08eqVYDafH6UjmztYRSJwL67aftxdTCyw1RSfWQlEW%25VxdSeZsNluzWQdjc9Fk3aeZQ6KownXRjcZwBL%25mrx6Kc%252yV34WwBKupCV1Fe%252B3inXZIjup2XH5rzv63iprJ849GwXHi4TPl29%25rJHFlMaxou4TJIsDA4L3FlTv0YLHyXIslGAtydCrv0s9OfwTE4GA0ogSkcNF9OALUdn%25kIogOybQ5mnvLUgChZS95GybUEqcdx89ChbNmKizPoEqhk7b1DMLNmqn1hdnDyk7m5VqQ7YCn178zmZetE5V1Pa7oDfN8zVMRcxSSkPazDxKBYdnMRaYWB2NQ5DxRte2pAZ8YWxfjpIBcPteWS6ivnKMfjedwHuXBDS6jQ%25JLn2Ydw6ZuTAnptQ%25wc3lOHifZu%25KXcagHS9uAqsr%25P61U39IjAeSw27BzcQkO",
+        ten: "THE FIRST-EVER XM",
+        loai: "Xăng",
+        gia: "Giá từ 10.999.000.000 VNĐ"
+    }
+]
+let str = "";
+
+for (let i = 0; i < product.length; i++) {
+    str += `
+    <div class="col-xl-3 col-lg-5 col-md-6 mt-5">
+            <div class="card">
+                <img src="${product[i].anh}"  class="card-img-top" alt="">
+                <div class="card-body">
+                    <h5 class="card-title" style="font-size: 24px;font-weight: 450">${product[i].ten}</h5>
+                    <h5 class="card-title" style="font-size: 15px;font-weight: 700 ">${product[i].loai}</h5>
+                    <h5 class="card-title" style="font-size: 20px;font-weight: 400">${product[i].gia}</h5>
+                    <a href="#" class="btn btn-outline-dark" style="background: none;border: none;font-weight: 700;font-size: 20px">>Hiển thị thêm</a>
+                </div>
+            </div>
+    </div>
+   `
+}
+document.getElementById("listproduct").innerHTML = str;
+
+let ads = [
+    {
+        img: "https://www.bmw.vn/content/dam/bmw/marketASIA/common/publicPools/teaser-pool/home/bmw-icss-home-spl-small.jpg/jcr:content/renditions/cq5dam.resized.img.485.low.time1560772316798.jpg",
+        nametag: "BÁN HÀNG DỰ ÁN VÀ NGOẠI GIAO"
+    },
+    {
+        img: "https://www.bmw.vn/content/dam/bmw/marketASIA/bmw_vn/topics/offers-and-services/bmw_connected_overview/overview-new/bmw-connected-drive-teaser-pool.jpg/jcr:content/renditions/cq5dam.resized.img.485.low.time1703575808290.jpg",
+        nametag: "BMW CONNECTED DRIVE"
+    },
+    {
+        img: "https://www.bmw.vn/content/dam/bmw/marketASIA/common/topics/offers-and-services/corporate-direct-sales/bmw-service-safety-vn-m.jpg/jcr:content/renditions/cq5dam.resized.img.485.low.time1588925457851.jpg",
+        nametag: "DỊCH VỤ AN TOÀN"
+    },
+    {
+        img: "https://www.bmw.vn/content/dam/bmw/marketASIA/common/topics/offers-and-services/corporate-direct-sales/bmw-service-inclusive-vn-m.jpg/jcr:content/renditions/cq5dam.resized.img.485.low.time1589276667828.jpg",
+        nametag: "DỊCH VỤ BẢO DƯỠNG ĐỊNH KỲ TRỌN GÓI"
+    },
+    {
+        img: "https://www.bmw.vn/content/dam/bmw/marketASIA/bmw_vn/PublicPools/teaser-pool/home/bmw-roadside-assistance-teaser.jpg/jcr:content/renditions/cq5dam.resized.img.485.low.time1615543179250.jpg",
+        nametag: "HỖ TRỢ SỰ CỐ TRÊN ĐƯỜNG"
+    },
+    {
+        img: "https://www.bmw.vn/content/dam/bmw/marketASIA/bmw_vn/PublicPools/teaser-pool/home/bmw-service-clinic-small-teaser.jpg/jcr:content/renditions/cq5dam.resized.img.485.low.time1621924349204.jpg",
+        nametag: "BẢO DƯỠNG LƯU ĐỘNG"
+    }
+]
+let arr = "";
+for (let i = 0; i < ads.length; i++) {
+    arr += `
+    <div class="col-xl-3 col-lg-5 col-md-6 mt-5">
+            <div class="card">
+                <img src="${ads[i].img}"  class="card-img-top" alt="">
+                <div class="card-body">
+                    <h2 class="card-title" style="font-size: 32px;font-weight: 350;height: 100px">${ads[i].nametag}</h2>
+                    <a href="#" class="btn btn-outline-dark" style="background: none;border: none;font-weight: 700;font-size: 20px;margin-top: 10px">>Khám phá thêm</a>
+                </div>
+            </div>
+    </div>
+    `
+}
+document.getElementById("ads").innerHTML = arr;
+
+let ctrl = [
+    {
+        icon: "https://icons.iconarchive.com/icons/microsoft/fluentui-emoji-mono/48/Information-icon.png",
+        name: "Đăng ký nhận thông tin"
+    },
+    {
+        icon: "https://icons.iconarchive.com/icons/custom-icon-design/mono-general-4/48/wrench-icon.png",
+        name: "Yêu cầu dịch vụ"
+    },
+    {
+        icon: "https://icons.iconarchive.com/icons/icons8/windows-8/48/Transport-Steering-Wheel-icon.png",
+        name: "Đăng ký lái thử BMW"
+    },
+    {
+        icon: "https://icons.iconarchive.com/icons/pictogrammers/material/48/human-greeting-variant-icon.png",
+        name: "Showroom BMW"
+    }
+]
+let sev = "";
+for (let i = 0; i < ctrl.length; i++) {
+    sev += `
+    <div class="col-xl-2 col-lg-4 col-md-6 mt-5 ml-5 mr-5">
+            <div class="card">
+                <img src="${ctrl[i].icon}"  class="card-img-top" alt="">
+                <div class="card-body">
+                    <h5 class="card-title" style="font-size: 18px;font-weight: 600">${ctrl[i].name}</h5>
+                </div>
+            </div>
+    </div>
+    `
+}
+document.getElementById("control").innerHTML = sev;
